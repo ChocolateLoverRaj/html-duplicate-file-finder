e7ddd4fd2e421f83a46e03c0e0ff6de7443a1075
Rajas Paranjpe <paranjperajas@gmail.com>
2021-04-22 18:13:12 -0500

    Cancel scan

--- pages/app.tsx
+++ pages/app.tsx
@@ -1,5 +1,5 @@
-import { Button, Card, Form, Progress, Table } from 'antd'
-import { FC, useState } from 'react'
+import { Button, Card, Form, Popconfirm, Progress, Table } from 'antd'
+import { FC, MouseEventHandler, useState } from 'react'
 import DirInput from '../components/DirInput'
 import NeedsFileAccessApi from '../components/NeedsFileAccessApi'
 
@@ -10,15 +10,17 @@ interface Scan {
 
 const AppPage: FC = () => {
   const [scans, setScans] = useState<Scan[]>([])
+  const [nextScanId, setNextScanId] = useState(0)
 
   const onFinish = ({ dir }: { dir: FileSystemDirectoryHandle }): void => {
     setScans([
       ...scans,
-      { 
-        id: scans.length,
+      {
+        id: nextScanId,
         dir
       }
     ])
+    setNextScanId(nextScanId + 1)
   }
 
   return (
@@ -31,7 +33,7 @@ const AppPage: FC = () => {
           <Button htmlType='submit' type='primary'>Scan Files</Button>
         </Form>
       </Card>
-      <Table 
+      <Table
         title={() => 'Scans'}
         columns={[
           {
@@ -41,11 +43,34 @@ const AppPage: FC = () => {
           },
           {
             title: 'Progress',
-            render: (_value, _record, _index) => <Progress />
+            render: (_value, _record, _index) => <Progress />,
+            width: '100%'
+          },
+          {
+            title: 'Actions',
+            render: (_value, _record, index) => {
+              const handleClick = (): void => {
+                setScans([
+                  ...scans.slice(0, index),
+                  ...scans.slice(index + 1)
+                ])
+              }
+
+              return (
+                <Popconfirm
+                  title='Are you sure you want to cancel this scan?'
+                  okText='Yes'
+                  cancelText='No'
+                  onConfirm={handleClick}
+                >
+                  <Button danger>Cancel Scan</Button>
+                </Popconfirm>
+              )
+            }
           }
         ]}
         dataSource={scans}
-        pagination={{hideOnSinglePage: true}}
+        pagination={{ hideOnSinglePage: true }}
       />
     </NeedsFileAccessApi>
   )
